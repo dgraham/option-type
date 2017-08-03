0bb9c286a7c259e3e192ca8841dafebf4947bc70
David Graham <david.malcom.graham@gmail.com>
2017-08-02 18:59:28 -0600

    Combine unwrap tests in a describe block

--- test/test.js
+++ test/test.js
@@ -13,13 +13,15 @@ describe('option', function() {
     assert(option.isNone());
   });
 
-  it('unwraps some value', function() {
-    const value = new Some(42);
-    assert.equal(value.unwrap(), 42);
-  });
+  describe('unwrap', function() {
+    it('unwraps some value', function() {
+      const value = new Some(42);
+      assert.equal(value.unwrap(), 42);
+    });
 
-  it('throws unwrapping none', function() {
-    assert.throws(() => new None().unwrap());
+    it('throws unwrapping none', function() {
+      assert.throws(() => new None().unwrap());
+    });
   });
 
   describe('unwrapOr', function() {
